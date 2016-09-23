e7c7498ff7be4cb3d1d4cfd11a1f2ec77940cac0
bright-tools <dev@brightsilence.com>
2016-09-23 19:43:01 +0100

    Closes #27 by preventing more than one instance of DeRange

--- DeRange/Program.cs
+++ DeRange/Program.cs
@@ -1,19 +1,31 @@
 ﻿using System;
+using System.Reflection;
+using System.Threading;
 using System.Windows.Forms;
 
 namespace DeRange
 {
     static class Program
     {
+        static Mutex mutex = new Mutex(true, "{"+ Assembly.GetExecutingAssembly().GetType().GUID.ToString() + "}");
+
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
         [STAThread]
         static void Main()
         {
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new DeRangeApp());
+            if (mutex.WaitOne(TimeSpan.Zero, true))
+            {
+                Application.EnableVisualStyles();
+                Application.SetCompatibleTextRenderingDefault(false);
+                Application.Run(new DeRangeApp());
+                mutex.ReleaseMutex();
+            }
+            else
+            {
+                MessageBox.Show("DeRange is already running.");
+            }
         }
     }
 }
