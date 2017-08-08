6f3b276dc1d54d7dd146dc186d6e0e5d31550b9a
bright-tools <dev@brightsilence.com>
2017-08-08 13:43:51 +0100

    Closes #29 by finishing about dialog

--- DeRange/Forms/About.Designer.cs
+++ DeRange/Forms/About.Designer.cs
@@ -28,23 +28,12 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.label1 = new System.Windows.Forms.Label();
             this.versionLabel = new System.Windows.Forms.Label();
             this.button1 = new System.Windows.Forms.Button();
             this.label2 = new System.Windows.Forms.Label();
             this.apacheLicense = new System.Windows.Forms.RichTextBox();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
             this.SuspendLayout();
-            // 
-            // label1
-            // 
-            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(189, 9);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(53, 13);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "DeRange";
             // 
             // versionLabel
             // 
@@ -87,6 +76,18 @@
             this.apacheLicense.Size = new System.Drawing.Size(404, 105);
             this.apacheLicense.TabIndex = 4;
             this.apacheLicense.Text = "";
+            this.apacheLicense.LinkClicked += new System.Windows.Forms.LinkClickedEventHandler(this.apacheLicense_LinkClicked);
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.Location = new System.Drawing.Point(189, 9);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(53, 13);
+            this.linkLabel1.TabIndex = 5;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "DeRange";
+            this.linkLabel1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked);
             // 
             // About
             // 
@@ -94,11 +95,11 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(408, 230);
+            this.Controls.Add(this.linkLabel1);
             this.Controls.Add(this.apacheLicense);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.versionLabel);
-            this.Controls.Add(this.label1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
             this.MaximizeBox = false;
             this.MinimizeBox = false;
@@ -110,11 +111,10 @@
         }
 
         #endregion
-
-        private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label versionLabel;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.RichTextBox apacheLicense;
+        private System.Windows.Forms.LinkLabel linkLabel1;
     }
 }
--- DeRange/Forms/About.cs
+++ DeRange/Forms/About.cs
@@ -18,6 +18,17 @@ namespace DeRange.Forms
 
             versionLabel.Text = System.Reflection.Assembly.GetExecutingAssembly().GetName().Version.ToString();
             apacheLicense.Rtf = global::DeRange.Properties.Resources.LICENSE;
+            linkLabel1.Links.Add(0, 7, "https://github.com/bright-tools/DeRange");
+        }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            System.Diagnostics.Process.Start(e.Link.LinkData.ToString());
+        }
+
+        private void apacheLicense_LinkClicked(object sender, LinkClickedEventArgs e)
+        {
+            System.Diagnostics.Process.Start(e.LinkText.ToString());
         }
     }
 }
